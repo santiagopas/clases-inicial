286dfd99bdd32185d7cf53702f3d96c512d6ad99
santiagopasciullo <santiagopasciullo@gmail.com>
2024-09-22 15:25:09 +0000

    plantilla inicial

--- counter.js
+++ counter.js
@@ -2,8 +2,9 @@ export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
     counter = count
-    element.innerHTML = `count is ${counter}`
+    element.innerHTML = `CLICKS: ${counter}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
+
 }
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { resetCounter } from './resetCounter.js' 
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,14 +12,16 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h2>Contador con JS!</h2>
     <div class="card">
       <button id="counter" type="button"></button>
+      <button id="reset" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Click en el logo de vite para aprender más
     </p>
   </div>
 `
 
 setupCounter(document.querySelector('#counter'))
+resetCounter(document.querySelector('#reset'))
